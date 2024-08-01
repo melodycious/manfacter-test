8aa281621776a248a6724e7052d9fac0708e3dc5
melodycious <melody.callejas@gmail.com>
2024-08-01 18:24:21 +0200

    button navbar AI quotation added

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import logo from ".//logo.png";
+import logo from './logo.png';
 import './navbar.css';
 
 const Navbar = () => {
-    return (
-        <>
-        <div >
-            <nav className="p-4 flex justify-between items-center">
-            <img src={logo} width="180" className="logo" />
-            <a href="/">Home</a>
-            <a href="/">Services</a>
-            <a href="/">Technologies And Materials</a>
-            <a href="/">Resources</a>
-            <a href="/">About Us</a>
-            <a href="/">Contact</a>
-            <a href="/">FAQ</a>
-        </nav>
-            </div>
-            
-        <div></div>
-        
-        </>
-    );
-  };
-  
-  export default Navbar;
+  return (
+    <nav className="p-4 flex justify-between items-center bg-white shadow-md">
+      <div className="flex items-center">
+        <img src={logo} width="180" className="logo" alt="Logo" />
+        <div className="ml-6 flex space-x-4">
+          <a href="/" className="text-gray-700 hover:text-blue-500">Home</a>
+          <a href="/" className="text-gray-700 hover:text-blue-500">Services</a>
+          <a href="/" className="text-gray-700 hover:text-blue-500">Technologies And Materials</a>
+          <a href="/" className="text-gray-700 hover:text-blue-500">Resources</a>
+          <a href="/" className="text-gray-700 hover:text-blue-500">About Us</a>
+          <a href="/" className="text-gray-700 hover:text-blue-500">FAQ</a>
+        </div>
+      </div>
+      <a href="/" className="px-4 py-2 bg-orange-500 text-white rounded-full hover:bg-orange-700">AI Quotation</a>
+    </nav>
+  );
+};
+
+export default Navbar;

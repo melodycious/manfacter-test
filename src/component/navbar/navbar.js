6ddcc7e0e5e9b890251f398677888dfa6b1d0695
melodycious <melody.callejas@gmail.com>
2024-08-01 18:15:10 +0200

    correcciones sidebar

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -2,20 +2,25 @@ import React from 'react';
 import logo from ".//logo.png";
 import './navbar.css';
 
-const Navbar = ({ toggleSidebar }) => {
+const Navbar = () => {
     return (
-      <nav className="p-4 flex justify-between items-center">
-        <button onClick={toggleSidebar} className="text-white">
-         <img src={logo} width="180" className="logo"/>
-        </button>
-                <a href="/">Home</a>
-                <a href="/">Services</a>
-                <a href="/">Technologies And Materials</a>
-                <a href="/">Resources</a>
-                <a href="/">About Us</a>
-                <a href="/">Contact</a>
-                <a href="/">FAQ</a>
-      </nav>
+        <>
+        <div >
+            <nav className="p-4 flex justify-between items-center">
+            <img src={logo} width="180" className="logo" />
+            <a href="/">Home</a>
+            <a href="/">Services</a>
+            <a href="/">Technologies And Materials</a>
+            <a href="/">Resources</a>
+            <a href="/">About Us</a>
+            <a href="/">Contact</a>
+            <a href="/">FAQ</a>
+        </nav>
+            </div>
+            
+        <div></div>
+        
+        </>
     );
   };
   
--- src/component/sidebar/sidebar.js
+++ src/component/sidebar/sidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { FaHome, FaShoppingCart, FaBoxOpen, FaTruck, FaUndo } from 'react-icons/fa';
+import { FaHome, FaShoppingCart, FaBoxOpen, FaTruck, FaUndo, FaRegQuestionCircle } from 'react-icons/fa';
+import { CgProfile } from "react-icons/cg";
+import { BiArrowToLeft } from "react-icons/bi";
 import whitelogo from '../sidebar/logoblanco.png';
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
@@ -14,30 +16,50 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         </button>
       </div>
       <nav className="flex flex-col flex-1 p-4">
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-blue-600 rounded">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
           <FaHome />
           <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Order</span>
         </a>
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-blue-600 rounded">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
           <FaShoppingCart />
           <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Manage cart</span>
         </a>
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-blue-600 rounded">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
           <FaBoxOpen />
           <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Unpaid</span>
         </a>
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-blue-600 rounded">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
           <FaTruck />
           <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Processing</span>
         </a>
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-blue-600 rounded">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
           <FaUndo />
           <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Shipped</span>
         </a>
-        <a href="#" className="flex items-center py-2 px-4 hover:bg-blue-600 rounded">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
           <FaUndo />
           <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Returns</span>
         </a>
+      </nav>
+
+      <nav className="align-bottom py-2 p-4">
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
+          <CgProfile />
+          <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Profile</span>
+        </a>
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
+          <FaRegQuestionCircle />
+          <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Need help?</span>
+        </a>
+        <a href="#" className="flex items-center py-2 px-4 hover:bg-white rounded hover:text-blue-800">
+            <BiArrowToLeft />
+            <button onClick={toggleSidebar} className="text-white hover:bg-white rounded hover:text-blue-800">
+            <span className={`${isOpen ? 'block' : 'hidden'} ml-4`}>Hide Sidebar</span>
+        </button>
+          
+        </a>
+        
+        
       </nav>
     </div>
   );
